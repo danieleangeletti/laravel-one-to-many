4652a6ab3b071ce55c5254f6c85926e2e10342bf
danieleangeletti <angelettidaniele@gmail.com>
2024-03-07 18:48:24 +0100

    ADD tutti i progetti relativi al type in admin.types.show

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -14,4 +14,24 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="text-center text-success">
+                        All projects associated with this type
+                    </h2>
+                    <ul>
+                        @foreach ($type->projects as $project)
+                            <li>
+                                <a href="{{ route('admin.projects.show', ['project' => $project->slug]) }}" class="link-offset-2 link-underline link-underline-opacity-0">
+                                    {{ $project->title }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
